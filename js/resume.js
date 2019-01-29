42db8c6e33c60506b1e31e22225948f68d2ea5f6
GTTeric <gametothrill.eric@gmail.com>
2019-01-29 14:42:19 -0800

    First commit

---
+++ js/resume.js
@@ -0,0 +1,55 @@
+(function($) {
+  "use strict"; // Start of use strict
+  
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#sideNav'
+  });
+
+  // Magnific popup calls
+  $('.popup-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1]
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+    }
+  });
+
+  // Displays spam-proof email by shrouding it in JS so crawlers can't extract it
+  /*
+  $('.spam-proof-email').click(function() {
+    var name = "ericfantillo"
+        domain = "outlook.com"
+        element = document.getElementById("em");
+
+    //&#64;
+    element.innerHTML = (name + '@' + domain);
+  }); */
+
+})(jQuery); // End of use strict
